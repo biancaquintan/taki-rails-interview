fb620a785c40053515183fea98f04c288ddcd72a
Mateus Oliveira <mateusodd@gmail.com>
2020-06-11 17:43:22 -0300

    adding tests

--- test/controllers/businesses_controller_test.rb
+++ test/controllers/businesses_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class BusinessesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    request.host = "locahost:3000"
+  end
+
+  test "should create business" do
+    log_in users(:one)
+    assert_difference('Business.count') do
+      post :create, business: { name: 'JJ Cafe', address: 'JL Gatot Subroto 25F', city: 'Jakarta Pusat', user_id: @user.id }
+    end
+    assert_redirected_to root_url
+  end
+
+  test "valid create business withou name" do
+    log_in users(:one)
+    assert_no_difference('Business.count') do
+      post :create, business: { address: 'JL Gatot Subroto 25F', city: 'Jakarta Pusat', user_id: @user.id }
+    end
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,25 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "valid user register" do
+  	assert User.new(email: 'test@taki.com', password: '000000', password_confirmation: '000000', first_name: 'John', last_name: 'Test').save
+  end
+
+  test "valid user without first name" do
+  	assert_not User.new(email: 'test@taki.com', password: '000000', password_confirmation: '000000', last_name: 'Test').save
+  end
+
+  test "valid user without last name" do
+  	assert_not User.new(email: 'test@taki.com', password: '000000', password_confirmation: '000000', first_name: 'John').save
+  end
+
+  test "valid user without email" do
+  	assert_not User.new(password: '000000', password_confirmation: '000000', first_name: 'John', last_name: 'Test').save
+  end
+
+  test "valid user without password" do
+  	assert_not User.new(email: 'test@taki.com', password_confirmation: '000000', first_name: 'John', last_name: 'Test').save
+  end
+
 end
